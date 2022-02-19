ae4df7a3597a2c565424c3869126ba3307c93c88
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-02-19 09:06:47 +0530

    Update __init__.py

--- webhook/__init__.py
+++ webhook/__init__.py
@@ -1,10 +1,10 @@
 import logging
-
+from datetime import datetime 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info(f'[I] App session started on {datetime.timestamp}')
 
     name = req.params.get('name')
     if not name:
