250e404d3072d06882b1b4f6a7e7b7211bca077c
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-02-23 22:00:05 +0530

    Class App initialized

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+import azure.functions as func
+
+
+class app:
+    _req: func.HttpRequest
+    _res: func.HttpResponse
+
+    def __init__(self, req: func.HttpRequest):
+        self._req = req
+
+    def map_requests(self):
+        pass
+
+    def execute(self) -> func.HttpResponse:
+        return self._res
+
 
 if __name__ == '__main__':
     print('Running main')
