d943a70143bcb09ad492f116da5989c802f02c23
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-02-23 21:56:44 +0530

    source files added

---
+++ src/jobs.py
@@ -0,0 +1,11 @@
+from bin.handlers import github_api_handler
+from azure  import functions as func
+
+class jobs:
+    def  __init__(self,req:func.HttpRequest) -> None:
+        pass
+
+    
+    
+    def map_request(self):
+        pass
