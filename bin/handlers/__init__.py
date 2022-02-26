f124b4da97a18f77652340870334bf80365513aa
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-02-26 11:44:05 +0530

    deleted all bin files due to name change

--- bin/handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .github_api_handler import *
-from .response_handler import *
-from .exception_handler import *
-from .authHandler import *
-from .files_handler import *
--- bin/utils.py
+++
@@ -1,8 +0,0 @@
-from bin.handlers import ResponseHandlers, Response
-from shlex import split
-
-
-def install_package(package):
-    args = split('pip3 install {} --retries'.format(package))
-    _r = ResponseHandlers.shell_response(command=args, output=False)
-    return Response.status_code
