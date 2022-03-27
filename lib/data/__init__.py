bcf043c4c78ed2400cde20e06097e64e5ed6515f
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-03-27 19:56:18 +0530

    Dataclass for GithubAPI added

---
+++ lib/data/__init__.py
@@ -0,0 +1,6 @@
+"""
+Data class library for Github Objects.
+Data classes are used to hold data extracted from API responses.
+"""
+
+from .githubDataClass import GithubRelease, GithubRefObject, GithubCommitObject, GithubTreeObject, GithubBlob, GithubPermissions, TreeType, AccessType, GitTree, GithubAppInstallations, Permission
