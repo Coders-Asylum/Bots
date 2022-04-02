6d0ca06947691778375c1092465b331d615a760d
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-04-02 21:07:51 +0530

    updated version

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='lib', version="0.0.1c", packages=['lib', 'lib.handlers','lib.data'])
+setup(name='lib', version="0.1.0a", packages=['lib', 'lib.handlers','lib.data'])
