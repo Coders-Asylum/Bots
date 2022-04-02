2793d1c924540b9e8d2476fdc0e75fcb161c1ff5
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-04-03 01:17:19 +0530

    renamed to match semantic standard for test files

--- tests/lib_tests/data_tests/test_githubDataClass.py
+++ tests/lib_tests/data_tests/test_githubDataClass.py
@@ -1,6 +1,7 @@
 from unittest import main, TestCase
 
-from lib.data.githubDataClass import Repository
+from lib.data.githubDataClass import *
+from tests.mocks.github_api_mocks import GithubAPIMock, Status
 
 
 class Repository_tests(TestCase):
@@ -21,6 +22,17 @@ class Repository_tests(TestCase):
         self.assertEqual(repo['owner'], self.owner)
         self.assertEqual(repo['branch'], self.branch)
         self.assertEqual(repo['name'], self.repo)
+
+
+class GithubTag_tests(TestCase):
+    g_api_mock: GithubAPIMock = GithubAPIMock(for_status=Status.SUCCESS)
+    expected_tag = loads(g_api_mock.get_tag().data)[0]
+
+    def test_data_class(self):
+        actual: GithubTag = GithubTag(self.expected_tag)
+
+        self.assertEqual(actual.name, self.expected_tag['name'])
+        self.assertEqual(actual.commit, self.expected_tag['commit'])
 
 
 if __name__ == "__main__":
