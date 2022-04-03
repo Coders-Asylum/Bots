0e8306c3487dba1ae79650c2d01d68148c2fc856
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-04-03 19:10:02 +0530

    GithubCommit added to imports

--- lib/data/__init__.py
+++ lib/data/__init__.py
@@ -3,5 +3,5 @@ Data class library for Github Objects.
 Data classes are used to hold data extracted from API responses.
 """
 
-from .githubDataClass import GithubRelease, GithubRefObject, GitCommit, GithubTreeObject, GithubBlob, GithubPermissions, TreeType, AccessType, GitTree, GithubAppInstallations, Permission, Repository, Webhook, GithubTag
+from .githubDataClass import GithubRelease, GithubRefObject, GitCommit, GithubTreeObject, GithubBlob, GithubPermissions, TreeType, AccessType, GitTree, GithubAppInstallations, Permission, Repository, Webhook, GithubTag, GithubCommit
 from .constants import Message
