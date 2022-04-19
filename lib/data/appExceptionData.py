15af13b33865f513f4c99afc95a6087a9070d3d7
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-04-19 23:28:56 +0530

    App exception data class

--- lib/data/appExceptionData.py
+++ lib/data/appExceptionData.py
@@ -2,25 +2,32 @@ from enum import Enum
 from colorama import Fore
 import logging as log
 
+from . import Message
 from ..handlers.response_handler import Response
 
 
 class ExceptionType(Enum):
-    CLOSE = Fore.RED + '[!] Program execution stopped due to a fatal error.'
-    WARNING = Fore.YELLOW + '[W] '
+    CLOSE = Fore.RED + 'Program execution stopped due to a fatal error.'
+    WARNING = Fore.YELLOW
+    ERROR = Fore.RED
 
 
 class AppException(Exception):
     """ Exception class to handle various exception raised during app runtime.
     """
 
-    def __int__(self, **kwargs):
-        super().__init__(self, kwargs['msg'])
-        self.__response = kwargs['response']
+    __msg: Message = Message()
 
-    @staticmethod
-    def log(msg):
+    def __init__(self, **kwargs):
+        self.__response: Response = kwargs['response']
+        self.__error_type: ExceptionType = kwargs['error_type']
+        self.log(kwargs['msg'])
+        super().__init__(self, kwargs['msg'])
+
+    def log(self, msg):
         """ Logs exception message into logger.
+
+        **Note: all logging is done under `root`.**
 
         Args:
             msg: exception message that has to be logged
@@ -28,7 +35,14 @@ class AppException(Exception):
         Returns: None
 
         """
-        log.exception(msg=Fore.RED + msg, stack_info=True)
+        if self.__error_type is ExceptionType.ERROR:
+            log.exception(self.__error_type.value + msg + f'\n{self.__msg.seperator}\n' + Fore.MAGENTA, stack_info=True)
+        elif self.__error_type is ExceptionType.WARNING:
+            log.warning(self.__error_type.value + msg + f'\n{self.__msg.seperator}\n' + Fore.MAGENTA, stack_info=True)
+        elif self.__error_type is ExceptionType.CLOSE:
+            log.critical(self.__error_type.value + f'\n Reason: {msg}' + f'\n{self.__msg.seperator}\n' + Fore.MAGENTA, stack_info=True)
+        else:
+            log.warning(ExceptionType.WARNING.value + f' {msg}' + f'\n{self.__msg.seperator}\n')
 
     @property
     def response(self):
@@ -42,13 +56,13 @@ class AppException(Exception):
 
 class GithubAppApiException(AppException):
 
-    def __int__(self, msg: str, api: str, response: Response):
-        super(GithubAppApiException, self).__int__(msg=msg, response=response)
-        super().log(msg=f'{msg} \n Response data from App {api} api: \n\t {response.status_code} {response.status} \n\t{response.data}')
+    def __init__(self, msg: str, api: str, response: Response, error_type: ExceptionType = ExceptionType.WARNING):
+        __msg = f'{msg} \n Response data from App {api} api: \n\t {response.status_code} {response.status} \n\t{response.data}'
+        super().__init__(msg=__msg, error_type=error_type, response=response)
 
 
 class GithubApiException(AppException):
 
-    def __int__(self, msg: str, api: str, response: Response):
-        _msg = f''
-        super(GithubApiException, self).__int__(msg=msg, response=response)
+    def __init__(self, msg: str, api: str, response: Response, error_type: ExceptionType = ExceptionType.WARNING):
+        __msg = f'{msg} \n Response data from {api} api: \n\t {response.status_code} {response.status} \n\t{response.data}'
+        super().__init__(msg=__msg, error_type=error_type, response=response)
