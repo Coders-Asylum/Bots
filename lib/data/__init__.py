73e612d04f363ddd00d8a86eeb3d766a38babf5b
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-04-25 20:18:41 +0530

    exported GithubMilestone as part of data package

--- lib/data/__init__.py
+++ lib/data/__init__.py
@@ -3,6 +3,6 @@ Data class library for Github Objects.
 Data classes are used to hold data extracted from API responses.
 """
 
-from .githubDataClass import GithubRelease, GithubRefObject, GitCommit, GithubTreeObject, GithubBlob, GithubPermissions, TreeType, AccessType, GitTree, GithubAppInstallations, Permission, Repository, Webhook, GithubTag, GithubCommit
+from .githubDataClass import GithubRelease, GithubRefObject, GitCommit, GithubTreeObject, GithubBlob, GithubPermissions, TreeType, AccessType, GitTree, GithubAppInstallations, Permission, Repository, Webhook, GithubTag, GithubCommit, GithubMilestone
 from .constants import Message
 from .appExceptionData import GithubAppApiException, GithubApiException, ExceptionType
