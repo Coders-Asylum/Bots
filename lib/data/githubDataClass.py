a889912daa9554d0120ca1b7088290462ded8036
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2022-04-28 20:28:52 +0530

    GithubIssue data class added

--- lib/data/githubDataClass.py
+++ lib/data/githubDataClass.py
@@ -207,3 +207,19 @@ class GithubMilestone(object):
         self.id = data['id']
         self.name = data['title']
         self.number = data['number']
+
+
+class GithubIssue(object):
+    id: int
+    url: str
+    number: int
+    state: str
+    title: str
+
+    def __init__(self, data: str):
+        _j = loads(data)
+        self.id = _j['id']
+        self.url = _j['url']
+        self.number = _j['number']
+        self.state = _j['state']
+        self.title = _j['title']
