0f2aa90d3fb59b3973faae6773f95f8b276b0342
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2023-12-24 23:18:36 +0530

    patch: test function endpoint created

--- sachiv-aatmaram-bhidu.cs
+++ sachiv-aatmaram-bhidu.cs
@@ -5,16 +5,11 @@ using Microsoft.Extensions.Logging;
 
 namespace Sachiv.Function
 {
-    public class sachiv_aatmaram_bhidu
+    public class Sachiv_Aatmaram_Bhidu(ILoggerFactory loggerFactory)
     {
-        private readonly ILogger _logger;
+        private readonly ILogger _logger = loggerFactory.CreateLogger<Sachiv_Aatmaram_Bhidu>();
 
-        public sachiv_aatmaram_bhidu(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<sachiv_aatmaram_bhidu>();
-        }
-
-        [Function("sachiv_aatmaram_bhidu")]
+        [Function("sachiv")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
