5d825323337ba9967c7a1976ece5fc16bfc0f4a1
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2023-12-24 15:36:09 +0530

    del: deleting all the py files

--- lib/data/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Data class library for Github Objects.
-
-Data classes are used to hold data extracted from API responses.
-"""
-
-from .githubDataClass import GithubRelease, GithubRefObject, GitCommit, GithubTreeObject, GithubBlob, GithubPermissions, TreeType, AccessType, GitTree, GithubAppInstallations, Permission, Repository, Webhook, GithubTag, GithubCommit, GithubMilestone, GithubIssue
-from .constants import Message, BotConfig, Status
-from .appExceptionData import GithubAppApiException, GithubApiException, ExceptionType
