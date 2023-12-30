7533741d9d20c742029e9e658a023f5cddaa79c9
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2023-12-30 17:29:35 +0530

    patch: not appending base dir

--- .tool/scripts/get_coverage_path.sh
+++ .tool/scripts/get_coverage_path.sh
@@ -6,15 +6,13 @@ baseDir="tests/results"
 # list all directories and subdirectories
 ls -R $baseDir
 
-ls -R
-
 # Capture all directories.
 dirs=$(ls -d $baseDir/*)
 
 # Extract the latest directory using regex matching based on modification time
 latestGuidDir=$(echo "$dirs" | sort -r | head -n1)
 
-coverageFilePath="./tests/results/$latestGuidDir/coverage.xml"
+coverageFilePath="./$latestGuidDir/coverage.xml"
 
 # print the coverage file path.
 echo $coverageFilePath
