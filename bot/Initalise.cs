47e7d6e619328c8f19eaf68d3e2efa482a4caccd
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2024-01-01 17:48:07 +0530

    patch: removed sentry package

--- bot/Initalise.cs
+++ bot/Initalise.cs
@@ -1,24 +1,8 @@
-using Sentry;
+
+using Castle.Core.Logging;
+using Microsoft.Extensions.Logging;
 
 namespace Bot.Initialise
 {
 
-    public class Sentry
-    {
-
-        static Sentry()
-        {
-
-            // Initialise the bot
-            // Initialise the database
-            // Initialise the commands
-            // Initialise the events
-            // Initialise the listeners
-            // Initialise the API
-            // Initialise the webserver
-            // Initialise the websockets
-
-            // Initialise the database
-        }
-    }
 }
