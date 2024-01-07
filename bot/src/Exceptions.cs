eb642c0fc90041a03913efa35e4f041dfd23d6d9
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2024-01-07 11:04:30 +0530

    add: Appmodule exception added.

--- bot/src/Exceptions.cs
+++ bot/src/Exceptions.cs
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file.
 
+using System.Drawing.Text;
+using Bots.Src.Utils;
 using Microsoft.Extensions.Logging;
 
 namespace Bot.Exceptions
@@ -82,6 +84,33 @@ namespace Bot.Exceptions
             return _errorResponse;
         }
 
+    }
+
+
+    /// <summary>
+    ///   Exceptions caught with the module and function details.
+    ///   Use this class to throw exceptions from within module and function details.
+    /// <example>
+    ///    <code>
+    ///    throw new ModuleException("MODULE_NAME", "FUNCTION_NAME", "ERROR_MESSAGE", exception);
+    ///    </code>
+    ///  </example>
+    ///</summary>
+    /// <remarks>
+    ///  <para>
+    ///  This class is used to throw exceptions from within module and function details.
+    ///  </para>
+    ///  <para>
+    /// <remarks>
+    ///   Constructs a new instance of the <see cref="AppModuleException"/> class.
+    /// </remarks>
+    /// <param name="module">The module name.</param>
+    /// <param name="function">The function name.</param>
+    /// <param name="message">The error message.</param>
+    /// <param name="error">The actual captured internal error,if any</param>
+    /// </summary>
+    public class AppModuleException(string module, string function, string message, Exception? error) : AppException(ErrorCodes.InternalError, $"[{module}][{function}]:{message}", error, HTTPStatus.INTERNAL_SERVER_ERROR, new Logger<AppModuleException>(new LoggerFactory()))
+    {
     }
 
 }
