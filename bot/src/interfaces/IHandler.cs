c06d710c34e09fd482fe1b8280f95703bab35811
Adithya Shetty <32545664+Maverick099@users.noreply.github.com>
2024-03-24 21:16:18 +0530

    wip

---
+++ bot/src/interfaces/IHandler.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2024 the CODERS Asylum project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+namespace Bot.Src.Interfaces
+{
+    /// <summary>
+    /// Interface that all the Hnadlers must implement.
+    /// </summary>
+    public interface IHandler
+    {
+    }
+}
